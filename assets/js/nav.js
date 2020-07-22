608c896ce5ef64bbddb4d27ac15d9e0579c13c99
Alex Ian Smith <taxihelms0@gmail.com>
2020-07-22 15:59:58 -0500

    remove responsive nav stuff

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,29 +1,29 @@
-let nav = document.getElementById("myTopnav");
-let brandName = document.getElementById("brand").innerHTML;
-let inits;
-function init() {
-  let initials = [];
-  initials[0] = (brandName[0]);
-  /* convert title to initials on resize */
-  for(let i=0; i<brandName.length; i++){
-    if(brandName[i] == " " && brandName[i+1]){
-      initials.push(brandName[i+1])
-    }
-  }
-  initials.push(".");
-  return initials;
-}
+// let nav = document.getElementById("myTopnav");
+// let brandName = document.getElementById("brand").innerHTML;
+// let inits;
+// function init() {
+//   let initials = [];
+//   initials[0] = (brandName[0]);
+//   /* convert title to initials on resize */
+//   for(let i=0; i<brandName.length; i++){
+//     if(brandName[i] == " " && brandName[i+1]){
+//       initials.push(brandName[i+1])
+//     }
+//   }
+//   initials.push(".");
+//   return initials;
+// }
     
 // function reportWindowSize() {
 //       // console.log(brandName);
 //       if (window.location.pathname != "/"){
 //         // show hamburger
 //         document.getElementById("hamburger").className = "icon";
-//         if (matchMedia('only screen and (max-width: 600px)').matches) {
-//           document.getElementById("brand").innerHTML = inits.join("");
-//         } else {
-//           document.getElementById("brand").innerHTML = brandName;
-//         }
+//         // if (matchMedia('only screen and (max-width: 600px)').matches) {
+//         //   document.getElementById("brand").innerHTML = inits.join("");
+//         // } else {
+//         //   document.getElementById("brand").innerHTML = brandName;
+//         // }
 //       } else {
 //         // hide hamburger
 //         document.getElementById("hamburger").className = "icon-hidden";
@@ -31,30 +31,30 @@ function init() {
       
 //     }
     
-function onLoad() {
+// function onLoad() {
       
-      // inits = init();
-      reportWindowSize();
+//       // inits = init();
+//       reportWindowSize();
 
-    }
-    // document.onload = onLoad();
-    window.onload = onLoad();
+//     }
+//     // document.onload = onLoad();
+//     window.onload = onLoad();
     
-    window.addEventListener('resize',() => {
-      reportWindowSize();
-    });
+//     window.addEventListener('resize',() => {
+//       reportWindowSize();
+//     });
   
-    /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-    /* toggle between initials and full site name */
-function dropDown() {
-      reportWindowSize();
-      if (nav.className === "topnav") {
-        nav.className += " responsive";
-        document.getElementById("myContent").className += " mobileMenu";
-        // document.getElementById("brand").innerHTML = brandName;
-      } else {
-        nav.className = "topnav";
-        document.getElementById("myContent").className = "content";
-        // document.getElementById("brand").innerHTML = inits.join("");
-      }
-}
+//     /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+//     /* toggle between initials and full site name */
+// function dropDown() {
+//       reportWindowSize();
+//       if (nav.className === "topnav") {
+//         nav.className += " responsive";
+//         document.getElementById("myContent").className += " mobileMenu";
+//         // document.getElementById("brand").innerHTML = brandName;
+//       } else {
+//         nav.className = "topnav";
+//         document.getElementById("myContent").className = "content";
+//         // document.getElementById("brand").innerHTML = inits.join("");
+//       }
+// }
