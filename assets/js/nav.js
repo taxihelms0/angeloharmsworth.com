b9b65361522266fa4b124a86e817f2fd73dc9192
Alex Ian Smith <taxihelms0@gmail.com>
2020-07-22 15:58:10 -0500

    removed inits on mobile

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -2,38 +2,38 @@ let nav = document.getElementById("myTopnav");
 let brandName = document.getElementById("brand").innerHTML;
 let inits;
 function init() {
-      let initials = [];
-      initials[0] = (brandName[0]);
-      /* convert title to initials on resize */
-      for(let i=0; i<brandName.length; i++){
-        if(brandName[i] == " " && brandName[i+1]){
-          initials.push(brandName[i+1])
-        }
-      }
-      initials.push(".");
-      return initials;
+  let initials = [];
+  initials[0] = (brandName[0]);
+  /* convert title to initials on resize */
+  for(let i=0; i<brandName.length; i++){
+    if(brandName[i] == " " && brandName[i+1]){
+      initials.push(brandName[i+1])
     }
+  }
+  initials.push(".");
+  return initials;
+}
     
-function reportWindowSize() {
-      // console.log(brandName);
-      if (window.location.pathname != "/"){
-        // show hamburger
-        document.getElementById("hamburger").className = "icon";
-        if (matchMedia('only screen and (max-width: 600px)').matches) {
-          document.getElementById("brand").innerHTML = inits.join("");
-        } else {
-          document.getElementById("brand").innerHTML = brandName;
-        }
-      } else {
-        // hide hamburger
-        document.getElementById("hamburger").className = "icon-hidden";
-      }
+// function reportWindowSize() {
+//       // console.log(brandName);
+//       if (window.location.pathname != "/"){
+//         // show hamburger
+//         document.getElementById("hamburger").className = "icon";
+//         if (matchMedia('only screen and (max-width: 600px)').matches) {
+//           document.getElementById("brand").innerHTML = inits.join("");
+//         } else {
+//           document.getElementById("brand").innerHTML = brandName;
+//         }
+//       } else {
+//         // hide hamburger
+//         document.getElementById("hamburger").className = "icon-hidden";
+//       }
       
-    }
+//     }
     
 function onLoad() {
       
-      inits = init();
+      // inits = init();
       reportWindowSize();
 
     }
